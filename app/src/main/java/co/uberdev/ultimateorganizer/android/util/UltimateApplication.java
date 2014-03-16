ecada687ba07cddf67f6b0dcc96c46ff368bfb2c
Oguz Bilgener <oguzbilgener@gmail.com>
2014-03-16 13:40:11 +0200

    add an application class

--- app/src/main/java/co/uberdev/ultimateorganizer/android/util/UltimateApplication.java
+++ app/src/main/java/co/uberdev/ultimateorganizer/android/util/UltimateApplication.java
@@ -1,7 +1,27 @@
 package co.uberdev.ultimateorganizer.android.util;
 
+import android.app.Application;
+import android.view.ViewConfiguration;
+
+import java.lang.reflect.Field;
+
 /**
  * Created by oguzbilgener on 16/03/14.
  */
-public class UltimateApplication {
+public class UltimateApplication extends Application {
+	@Override
+	public void onCreate() {
+		// A little hack to force to show overflow menu
+		// on devices with physical menu button
+		try {
+			ViewConfiguration config = ViewConfiguration.get(this);
+			Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+			if(menuKeyField != null) {
+				menuKeyField.setAccessible(true);
+				menuKeyField.setBoolean(config, false);
+			}
+		} catch (Exception ex) {
+			Utils.log.w("cannot force overflow menu");
+		}
+	}
 }
