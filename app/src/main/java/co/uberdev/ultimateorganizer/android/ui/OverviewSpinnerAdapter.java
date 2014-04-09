29ef65270cee735384bb424b4d7ffef86dd53db9
Oguz Bilgener <oguzbilgener@gmail.com>
2014-04-09 23:47:14 +0300

    some more progress

--- app/src/main/java/co/uberdev/ultimateorganizer/android/ui/OverviewSpinnerAdapter.java
+++ app/src/main/java/co/uberdev/ultimateorganizer/android/ui/OverviewSpinnerAdapter.java
@@ -11,22 +11,21 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 import co.uberdev.ultimateorganizer.android.R;
-import co.uberdev.ultimateorganizer.android.util.OverviewNavigationItem;
-import co.uberdev.ultimateorganizer.android.util.Utils;
 
 /**
  * Created by begum on 08/04/14.
  */
 
-public class OverviewSpinnerAdapter extends ArrayAdapter implements SpinnerAdapter {
+public class OverviewSpinnerAdapter extends ArrayAdapter<String> implements SpinnerAdapter {
 
-    private ArrayList<OverviewNavigationItem> list;
+    private ArrayList<String> list;
     private LayoutInflater inflater;
     public static final int resourceId = R.layout.overview_spinner_item;
+	public static final int resourceIdAlt = R.layout.overview_spinner_item;
 
-    public OverviewSpinnerAdapter(Context context, ArrayList<OverviewNavigationItem> list)
+    public OverviewSpinnerAdapter(Context context, ArrayList<String> list)
 	{
-        super(context, resourceId, list);
+        super(context, resourceId, R.id.overview_spinner_item_text, list);
 
         inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         this.list = list;
@@ -38,31 +37,81 @@ public class OverviewSpinnerAdapter extends ArrayAdapter implements SpinnerAdapt
     }
 
 	@Override
-    public View getView(int position, View convertView, ViewGroup parent)
+	 public View getView(int position, View convertView, ViewGroup parent)
+	{
+		return getItemView(position, convertView, parent);
+	}
+
+	@Override
+	public View getDropDownView(int position, View convertView, ViewGroup parent)
+	{
+		return getItemView(position, convertView, parent);
+	}
+
+    public View getItemView(int position, View convertView, ViewGroup parent)
     {
         if(position >= list.size())
             return null;
 
+		int resId;
+
+		if(getItemViewType(position) == 0)
+		{
+			resId = resourceId;
+		}
+		else
+		{
+			resId = resourceIdAlt;
+		}
+
         ViewHolder holder;
 
-        if(convertView == null)
-        {
-            convertView = inflater.inflate(R.layout.overview_spinner_item, parent, false);
+//        if(convertView == null)
+//        {
+            convertView = inflater.inflate(resId, parent, false);
 
             holder = new ViewHolder();
-			try {
-				holder.text = (TextView) convertView.findViewById(resourceId);
-			}catch(Exception e) {
-				Utils.log.d("hoooo"); e.printStackTrace();}
-            convertView.setTag(R.id.overview_spinner_holder, holder);
-        }
-        else
-        {
-            holder = (ViewHolder) convertView.getTag(R.id.overview_spinner_holder);
-        }
+			holder.text = (TextView) convertView.findViewById(resourceId);
+//            convertView.setTag(R.id.overview_spinner_holder, holder);
+//        }
+//        else
+//        {
+//            holder = (ViewHolder) convertView.getTag(R.id.overview_spinner_holder);
+//        }
 		if(holder.text != null)
-        	holder.text.setText(list.get(position).title);
+		{
+			if(getItemViewType(position) == 0)
+			{
+				holder.text.setText(list.get(position));
+			}
+			else
+			{
+				holder.text.setText("last");
+			}
+		}
 
         return convertView;
     }
+
+	/**
+	 * The last item is a complex view
+	 * @param position
+	 * @return
+	 */
+	@Override
+	public int getItemViewType(int position)
+	{
+		return position < list.size()-1 ? 0 : 1;
+	}
+
+	/**
+	 * We have two different item types for this Spinner. One is a simple text item,
+	 * the other one is a rather complex view
+	 * @return
+	 */
+	@Override
+	public int getViewTypeCount()
+	{
+		return 2;
+	}
 }
