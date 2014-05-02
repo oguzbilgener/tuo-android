5674a4a9978a7e91ece9562416973e8f157d7145
Oguz Bilgener <oguzbilgener@gmail.com>
2014-05-02 21:02:57 +0300

    all my changes too

---
+++ app/src/main/java/co/uberdev/ultimateorganizer/android/ui/BlockListAdapter.java
@@ -0,0 +1,62 @@
+package co.uberdev.ultimateorganizer.android.ui;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import co.uberdev.ultimateorganizer.android.R;
+
+/**
+ * Created by oguzbilgener on 02/05/14.
+ */
+public class BlockListAdapter extends ArrayAdapter
+{
+	private Context context;
+	private List<?> items;
+	private LayoutInflater inflater;
+	private int resourceId;
+	public BlockListAdapter(Context context, int resource, List<?> objects)
+	{
+		super(context, resource, objects);
+		this.context = context;
+		inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		resourceId = resource;
+		items = objects;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent)
+	{
+		ViewGroup itemView = (ViewGroup) inflater.inflate(resourceId, parent, false);
+
+		Spinner reminderDateSpinner = (Spinner) itemView.findViewById(R.id.spinner_reminder_date);
+		Spinner reminderTypeSpinner = (Spinner) itemView.findViewById(R.id.spinner_reminder_type);
+
+		reminderDateSpinner.setAdapter(new ReminderDateSpinnerAdapter(context, android.R.layout.simple_list_item_1, new ArrayList<String>()));
+		reminderTypeSpinner.setAdapter(new ReminderTypeSpinnerAdapter(context, android.R.layout.simple_list_item_1, new ArrayList<String>()));
+
+		return itemView;
+	}
+
+	public class ReminderDateSpinnerAdapter extends ArrayAdapter<String>
+	{
+
+		public ReminderDateSpinnerAdapter(Context context, int resource, List<String> objects) {
+			super(context, resource, objects);
+		}
+	}
+
+	public class ReminderTypeSpinnerAdapter extends ArrayAdapter<String>
+	{
+
+		public ReminderTypeSpinnerAdapter(Context context, int resource, List<String> objects) {
+			super(context, resource, objects);
+		}
+	}
+}
