8ffcdf0f39cf84ec1fb0d9c8dc02cb90245c9b16
Oguz Bilgener <oguzbilgener@gmail.com>
2014-05-06 16:59:14 +0300

    add latest client updates to android project

--- app/src/main/java/co/uberdev/ultimateorganizer/client/TuoClient.java
+++ app/src/main/java/co/uberdev/ultimateorganizer/client/TuoClient.java
@@ -1,18 +1,15 @@
 package co.uberdev.ultimateorganizer.client;
 
-import co.uberdev.ultimateorganizer.core.CoreCrypto;
-import co.uberdev.ultimateorganizer.core.CoreDataRules;
-import co.uberdev.ultimateorganizer.core.CoreJSON;
+import co.uberdev.ultimateorganizer.core.*;
 
 import java.io.IOException;
 
 /**
-* Created by ata on 4/9/14.
-*/
+ * Created by ata on 4/9/14.
+ */
 public class TuoClient{
 
 
-    boolean logged;
     private String publicKey;
     private String secretToken;
 
@@ -30,13 +27,13 @@ public class TuoClient{
 
     /**
      * Get ready to make a request to server
-     * @param endpoint
+     * @param endPoint
      * @param requestData
      * @param httpMethod
      * @param loggedIn
      * @return
      */
-    public APIRequest prepareRequest(String endpoint, String requestData, String httpMethod, boolean loggedIn)
+    public APIRequest prepareRequest(String endPoint, String requestData, String httpMethod, boolean loggedIn)
     {
 
 
@@ -44,15 +41,15 @@ public class TuoClient{
         if(loggedIn)
         {
             String signature = CoreCrypto.hmacSha1(requestData, secretToken);
-            endpoint += "?"+ CoreDataRules.fields.request.public_key + "=" + publicKey + "&" + CoreDataRules.fields.request.signature + "=" + signature;
+            endPoint += "?"+ CoreDataRules.fields.request.public_key + "=" + publicKey + "&" + CoreDataRules.fields.request.signature + "=" + signature;
         }
 
-        return new APIRequest(endpoint, requestData,httpMethod);
+        return new APIRequest(endPoint, requestData,httpMethod);
     }
 
 
     /**
-     *Login with given parameters. Returs the result as an object.
+     *Login with given parameters. Returns the result as an object.
      * @param email
      * @param password
      * @return
@@ -67,7 +64,8 @@ public class TuoClient{
 
         String jsonString = data.getAsJsonString();
 
-        co.uberdev.ultimateorganizer.client.APIRequest request = prepareRequest("/auth/login", jsonString, "POST", false);
+        APIRequest request = prepareRequest("/auth/login", jsonString, "POST", false);
+
         return request.run();
     }
 
@@ -96,7 +94,7 @@ public class TuoClient{
 
         String jsonString = data.getAsJsonString();
 
-        co.uberdev.ultimateorganizer.client.APIRequest request = prepareRequest("/auth/register", jsonString, "POST", false);
+        APIRequest request = prepareRequest("/auth/register", jsonString, "POST", false);
 
         return request.run();
     }
@@ -106,6 +104,63 @@ public class TuoClient{
     {
         // hede hodo
         return null;
+    }
+
+
+    public APIResult insertTask(CoreTask toAdd) throws IOException
+    {
+
+        APIRequest request = prepareRequest("/tasks/insert", toAdd.asJsonString(), "POST", true);
+
+        return request.run();
+
+    }
+
+    public APIResult updateTask(CoreTask toUpdate) throws IOException
+    {
+        toUpdate.setLastModified(CoreUtils.getUnixTimestamp());
+        APIRequest request = prepareRequest("/tasks/update", toUpdate.asJsonString(), "POST", true);
+
+        return request.run();
+    }
+
+    public APIResult removeTask(CoreTask toRemove) throws IOException
+    {
+        APIRequest request = prepareRequest("/tasks/remove", toRemove.asJsonString(), "POST", true);
+
+        return request.run();
+    }
+
+    public APIResult insertNote(CoreNote toAdd) throws IOException
+    {
+        APIRequest request = prepareRequest("/notes/insert", toAdd.asJsonString(), "POST", true);
+
+        return request.run();
+
+    }
+
+    public APIResult updateNote(CoreNote toUpdate) throws IOException
+    {
+        toUpdate.setLastModified(CoreUtils.getUnixTimestamp());
+        APIRequest request = prepareRequest("/notes/update", toUpdate.asJsonString(), "POST", true);
+
+
+        return request.run();
+    }
+
+    public APIResult removeNote(CoreNote toRemove) throws IOException
+    {
+        APIRequest request = prepareRequest("/notes/remove", toRemove.asJsonString(), "POST", true);
+
+        return request.run();
+    }
+
+    public APIResult verifyTask(String msg) throws IOException {
+        CoreJSON json = new CoreJSON();
+        json.put("message", msg);
+        APIRequest request = prepareRequest("/verify", json.getAsJsonString(), "POST", true);
+
+        return request.run();
     }
 
 
